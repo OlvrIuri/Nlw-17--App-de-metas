a4871a0c109e46975395dde4910f6c8fc70e6430
Iuri Oliveira <iuri.oliveira099@gmail.com>
2024-09-11 09:35:43 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -56,10 +56,26 @@ const metasRealizadas = async () =>{
     }
 
     await select({
-        message: 'Metas realizadas',
+        message: 'Metas realizadas' + realizadas.length,
         choices: [...realizadas]
     })
 }
+
+const metasAbertas = async () => {
+    const abertas =  metas.filter((meta) => {
+        return meta.checked != true 
+    })
+
+    if(abertas.length == 0) {
+        console.log('Não existem metas abertas :)')
+        return
+    }
+    await select({
+        message: 'Metas abertas',
+        choices: [...metas]
+        })
+    }
+
 const  start = async() => {
     
     while(true){
@@ -80,6 +96,10 @@ const  start = async() => {
                     value:'realizadas'
                 },
                 {
+                    name:'Metas abertas',
+                    value:'abertas'
+                },
+                {
                     name : 'Sair',
                     value: 'sair'
                 }
@@ -96,6 +116,9 @@ const  start = async() => {
                 break
             case 'realizadas':
                 await metasRealizadas()
+                break
+            case 'abertas':
+                await metasAbertas()
                 break
             case 'sair':
                 console.log('Até a próxima!')
